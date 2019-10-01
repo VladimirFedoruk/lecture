a86572422b954239e0f446c7b1156a82b316b78d
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-01 21:12:49 +0300

    add Task1.java
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

--- gameMario/src/Exceptions/Task1.java
+++ gameMario/src/Exceptions/Task1.java
@@ -1,4 +1,24 @@
 package Exceptions;
-
+//Иерархия класов исключений.
+// 1. Группа Eror  и все его наследники.
+// 2. Группа Exception  и все его наследники.
+// 3. Группа RuntimeException и его наследники.
+class Goodoo{
+    void doStuff(){
+        doMore();
+    }
+    void doMore(){
+        throw new ArithmeticException();
+    }
+}
 public class Task1 {
+    public static void main(String args[]){
+        Goodoo goodOO= new Goodoo();
+        try {
+            goodOO.doStuff();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+//          Logger.getLogger(Task01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
