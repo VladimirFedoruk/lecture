5bf2cb4bcc6e31a7d5073091ad4576abee4b18c4
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-15 19:19:41 +0300

    добавил перегруженный метод
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

---
+++ Collections/src/redefineMetod.java
@@ -0,0 +1,16 @@
+
+class Animal{
+    void eat(){
+        System.out.println("lkjlkj");
+    }
+    void findAndEat(){
+        eat();
+        super.eat():
+
+    }
+}
+public class redefineMetod {
+    public static void main(String[] args) {
+
+    }
+}
