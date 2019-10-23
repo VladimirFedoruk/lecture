198addeeae68d1e9b79ef8982abc4d78a2b32d3a
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-24 00:08:34 +0300

    игра
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -1,2 +1,9 @@
+import java.util.Arrays;
+
 public class HiLo {
+    public static void main(String[] args) {
+        //генерация случайного числа котору будет унпдывать пользователь
+        int theNamber = (int)(Math.random()*100 +1);
+        System.out.println(theNamber);
+    }
 }
