c5abc9d076e87d819313db0a26df414e8b25fbf3
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-24 22:57:55 +0300

    add Scanner
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -1,9 +1,15 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HiLo {
     public static void main(String[] args) {
-        //генерация случайного числа котору будет унпдывать пользователь
+        Scanner scan = new Scanner(System.in);
+        //генерация случайного числа которую будет угадывать пользователь
         int theNamber = (int)(Math.random()*100 +1);
         System.out.println(theNamber);
+        int guess = 0;
+        System.out.println("Guess a number between 1 and 100:");
+        guess = scan.nextInt();
+        System.out.println("You entered " + guess + ".");
     }
 }
