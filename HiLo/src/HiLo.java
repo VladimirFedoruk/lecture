d8cf54389289f64d6bd251572a3e4f0b68198473
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-24 23:18:03 +0300

    add cykl
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -4,12 +4,24 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String playAgain = "";
+        do {
         //генерация случайного числа которую будет угадывать пользователь
-        int theNamber = (int)(Math.random()*100 +1);
-        System.out.println(theNamber);
+        int theNumber = (int) (Math.random() * 100 + 1);
+        System.out.println(theNumber);
         int guess = 0;
-        System.out.println("Guess a number between 1 and 100:");
-        guess = scan.nextInt();
-        System.out.println("You entered " + guess + ".");
+        while (guess != theNumber) {
+            System.out.println("Guess a number between 1 and 100:");
+            guess = scan.nextInt();
+            if (guess < theNumber)
+                System.out.println(guess + " is too low. Try again.");
+                else if (guess > theNumber)
+                System.out.println(guess + " is too high. Try again.");
+            else
+                System.out.println(guess + " is correct. You win!");
+        }
+            System.out.println("Would you like to play again (y/n)?");
+            playAgain = scan.next();
+        } while (playAgain.equalsIgnoreCase("y"));
     }
 }
