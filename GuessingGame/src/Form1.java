a75b1345e8033a492ade50b27657441c531a6a3a
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-27 22:56:52 +0300

    add formu
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

---
+++ GuessingGame/src/Form1.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+
+public class Form1 {
+    public Container panel1;
+}
---
+++ GuessingGame/src/GuessingGame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+
+public class GuessingGame extends JFrame {
+    public static void main(String[] args) {
+
+    }
+}
