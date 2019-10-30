12af9e2882bfb3f8b07045af146a8854be06d0f3
VladimirFedoruk <anvil.prog@gmail.com>
2019-10-30 20:46:25 +0300

    add new game
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -6,6 +6,9 @@ import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Dimension;
 
 public class GuessingGame extends JFrame {
 	private JTextField txtGuess;
@@ -15,27 +18,30 @@ public class GuessingGame extends JFrame {
 		String guessText = txtGuess.getText();
 		String message = "";
 		int guess = Integer.parseInt(guessText);
-		if (guess < theNumber)
-			message = + " это число меньше. Введите ещё раз";
+		if (guess < theNumber) 
+			message = "Ты шо? " + guess + " это маловато. Давай шэ разок";
 		else if (guess > theNumber)
-		    message = + " это число больше. Введите ещё раз";
+		    message = " Э не "+ guess + " то слишком много. А ну, шэ раз";
 		else 
-			message = guess + " Ого! Правильно! Чувак, ты выиграл!";
+			message = guess + " Да ты шо! Вгадав! Чувак, ты выиграл!";
 		lblOutput.setText(message);
+	}
+	public void newGame() {
+		theNumber = (int)(Math.random() * 100 + 1);
 	}
 	public GuessingGame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("MAX Evaluation 01");
 		getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Введите данные автопоезда с грузом, что бы MAX все сделал правильно ");
+		JLabel lblNewLabel = new JLabel("Привет Саша Федорук, ну шо угадаеш чысло?");
 		lblNewLabel.setForeground(Color.RED);
-		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 14));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(12, 12, 468, 15);
 		getContentPane().add(lblNewLabel);
 		
-		JLabel label = new JLabel("Угадай число от 1 до 100:");
+		JLabel label = new JLabel("Я загадав от 1 до 100:");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
 		label.setBounds(66, 49, 295, 15);
 		getContentPane().add(label);
@@ -43,20 +49,27 @@ public class GuessingGame extends JFrame {
 		txtGuess = new JTextField();
 		txtGuess.setBounds(366, 39, 114, 25);
 		getContentPane().add(txtGuess);
-		txtGuess.setColumns(10);
+		txtGuess.setColumns(12);
 		
 		JButton button = new JButton("Ввести");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				checkGuess();
+			}
+		});
 		button.setBounds(366, 171, 114, 25);
 		getContentPane().add(button);
 		
 		lblOutput = new JLabel("Введите число и нажмите кнопку");
 		lblOutput.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblOutput.setBounds(72, 176, 289, 20);
+		lblOutput.setBounds(24, 176, 337, 20);
 		getContentPane().add(lblOutput);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GuessingGame theGame = new GuessingGame();
+		theGame.newGame();
+		theGame.setSize(new Dimension(600,400));
+		theGame.setVisible(true);
 	}
 }
