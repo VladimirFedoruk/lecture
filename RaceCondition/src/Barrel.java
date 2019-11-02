b3891059a4d23d0e5fbc9806aff7c07dbd21a8e8
VladimirFedoruk <anvil.prog@gmail.com>
2019-11-02 14:22:42 +0300

    add race condition
    Signed-off-by: VladimirFedoruk <anvil.prog@gmail.com>

---
+++ RaceCondition/src/Barrel.java
@@ -0,0 +1,31 @@
+public class Barrel implements Runnable {
+    private int value;
+    String e = "Empty";
+    String f = "full";
+    public void useTank( ) {
+        value= (int) (Math.random() * 10)*10;
+        if (value>0){
+            System.out.println(f+" "+value);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }else{
+            System.out.println(e+""+value);
+        }
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            useTank();
+        }
+    }
+    public static void main(String[] args) {
+        Barrel barrel= new Barrel();
+        Thread tr1 = new Thread(barrel);
+        Thread tr2 = new Thread(barrel);
+        tr1.start();
+        tr2.start();
+    }
+}
